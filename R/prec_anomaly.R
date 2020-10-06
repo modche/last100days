3e9c10b409697e1f330f3ae508e28191d383a80e
Michael Stölzle <michaelstoelzle@me.com>
2020-10-06 16:36:35 +0200

    update manuals

--- R/prec_anomaly.R
+++ R/prec_anomaly.R
@@ -3,19 +3,20 @@
 #' @description Find exact station names with \code{rdwd::findID("Berlin", exactmatch=FALSE)}.
 #'
 #' @param station Station name from DWD (German Weather Service)
-#' @param days How many days (backweards from today) should be considered? Delaut = 100 days.
+#' @param days How many days (backwards from today) should be considered? Default = 100 days.
+#'      Values smaller than 14 days not recommended.
+#' @param quiet If \code{TRUE} (default) messages from \code{rdwd} are suppressed.
 #'
 #' @return A table. tbc.
 #' @export
 #'
-#' @references See detailed station list from DWD here:
-#'     ftp://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/daily/kl/historical/KL_Tageswerte_Beschreibung_Stationen.txt
+#' @references See detailed station list from DWD [here](ftp://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/daily/kl/historical/KL_Tageswerte_Beschreibung_Stationen.txt)
 #'
 #' @examples
-#' prec_anomaly("Freiburg")
+#' prec_anomaly("Freiburg", days = 365)
 #' prec_anomaly("Mannheim")
 #' prec_anomaly("Koeln-Bonn")
-#' prec_anomaly("Waltrop-Abdinghof")
+#' prec_anomaly("Waltrop-Abdinghof", days = 2000)
 prec_anomaly <- function(station, days = 100, quiet = TRUE){
 require(magrittr, quietly = TRUE)
 require(dplyr, quietly = TRUE)
@@ -25,8 +26,10 @@ require(lubridate, quietly = TRUE)
 station_dwd <- rdwd::selectDWD(station, res="daily", var="kl", per="hr")
 data <- rdwd::dataDWD(station_dwd, dir=tempdir(), quiet = quiet)
 
-
-
+if (days < 14) {
+	warning("Period is set to 14 days.")
+	days <- 14
+}
 
 df <- bind_rows(data[[1]], data[[2]]) %>%
 		tibble::as_tibble() %>%
@@ -34,14 +37,14 @@ df <- bind_rows(data[[1]], data[[2]]) %>%
 		padr::pad(interval = "days") %>%
 		janitor::clean_names()
 
-selected_doy <- lubridate::yday(tail(df$mess_datum,1))-1
+selected_doy <- lubridate::yday(tail(df$mess_datum,1))
 
-df2 <- df %>% select(mess_datum, rain=rsk) %>%
+df2 <- df %>% select(mess_datum, rain_daily=rsk) %>%
 	#filter(mess_datum > '1951-10-04 00:00:00') %>%
 	mutate(period = days) %>%
-	mutate(prec_period = zoo::rollsum(rain, days, fill=NA, align="right", na.rm=T)) %>%
+	mutate(prec_period = zoo::rollsum(rain_daily, days, fill=NA, align="right", na.rm=T)) %>%
 	mutate(doy=yday(mess_datum)) %>%
-	filter(doy == selected_doy) %>%
+	filter(doy == selected_doy, prec_period > 0) %>%
 	mutate(prec_mean = mean(prec_period, na.rm = TRUE)) %>%
 	mutate(ratio = prec_period / prec_mean)
 
@@ -50,5 +53,5 @@ df2 <- df %>% select(mess_datum, rain=rsk) %>%
 
 
 
-return(tail(df2))
+return(rbind(head(df2),sample_n(df2,size = 6),tail(df2)))
 }
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# last100days
-How abnormal was the weather in the last 100 days?
+# R package `last100days`
+
+> How abnormal was the weather in the last 100 days?
+
+## Introduction
+
+Do you know these statements: "Oh, this summer was the driest I have ever seen in my life!" or "The rainfall amount in the last two weeks is absolutely a new record".
+
+Hmm... I always have the impression that our memory is really biased towards recent weather events, i.e. every new season seems to be extreme in case of precipitation or temperature and a comparison with the past is seldom done.
+
+The R package `last100days` calculates the anomaly of some climate variables for a given location and a given period.
+
+-   climate variable: precipitation, (temperature)
+
+-   locations: German weather stations from the German Weather Service (DWD)
+
+The package uses the `rdwd` resources and other packages from the `tidyverse`.
